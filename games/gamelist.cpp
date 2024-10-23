b4486740bbbe32baaf2f465e9ef7d574c53ae10c
CMHarris14 <cainh1174+githubalt@gmail.com>
2024-10-23 09:29:42 -0400

    gamelist additions

--- games/gamelist.cpp
+++ games/gamelist.cpp
@@ -1,4 +1,5 @@
 #include "gamelist.h"
+#include "../util/logger.h"
 
 GameList::GameList() {}
 
@@ -18,4 +19,29 @@ QVector<QString> GameList::get_game_titles() {
         titles.push_back(game.title);
     }
     return titles;
+}
+
+std::optional<GameHandler> GameList::game_by_title(QString title) {
+    for (int i = 0; i < game_list.length(); i++) {
+        if (game_list[i].title == title) {
+            return game_list[i];
+        }
+    }
+    return std::nullopt;
+}
+
+std::optional<GameHandler> GameList::game_at(int index) {
+    if (index < game_list.length()) {
+        return game_list[index];
+    }
+    return std::nullopt;
+}
+
+void GameList::save_database() {
+    try {
+        SQLite::Database db("GameData.db3",
+                            SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+    } catch (SQLite::Exception e) {
+        Logger::log(Logger::ERROR, e.what());
+    }
 }
--- games/gamelist.h
+++ games/gamelist.h
@@ -3,7 +3,6 @@
 
 #include "../util/exceptions.cpp"
 #include "gamehandler.h"
-#include <iostream>
 
 #include <SQLiteCpp/SQLiteCpp.h>
 
@@ -17,15 +16,10 @@ class GameList {
     void add_game(QString title);
     // Returns the titles of every managed game
     QVector<QString> get_game_titles();
+    std::optional<GameHandler> game_by_title(QString title);
+    std::optional<GameHandler> game_at(int index);
 
-    void save_database() {
-        try {
-            SQLite::Database db("test.db3",
-                                SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
-        } catch (SQLite::Exception e) {
-            std::cout << e.what() << std::endl;
-        }
-    }
+    void save_database();
 };
 
 #endif // GAMELIST_H
